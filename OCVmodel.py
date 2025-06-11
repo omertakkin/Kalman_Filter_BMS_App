ddb72bfe3dee6b25996e06e7f48e862c737e4e6b
Ömer Takkin <omer.takkin@gmail.com>
2025-06-11 15:20:12 +0300

    modified:   OCVmodel.py 	modified:   model_out/sim_data.csv 	modified:   model_param/cell_model.csv

--- OCVmodel.py
+++ OCVmodel.py
@@ -6,29 +6,28 @@ import matplotlib.pyplot as plt
 dt = 1  # delta t in seconds
 sample_freq = 1 / dt
 sim_end_time = 6000  # in seconds
-curr_T = 5  # Index corresponding -> 25 degrees Celsius
+curr_T = 5  # [-25 -15 -5 5 15 25 45] degrees Celsius, index for temperature
 
 cell_model = pd.read_csv('model_param/cell_model.csv')
 
 # Fields pertaining to the OCV versus SOC relationship:
-OCV   = cell_model.iloc[:,0].dropna()  # OCV vector at which SOC0 and SOCrel are stored
-OCV0  = cell_model.iloc[:,1].dropna()  # Vector of OCV versus SOC at 0 degree Celsius
-OCVrel= cell_model.iloc[:,2].dropna()  # Vector of change in OCV versus SOC per degree Celsius [V/C]
-SOC   = cell_model.iloc[:,3].dropna()  # SOC vector at which OCV0 and OCVrel are stored
-SOC0  = cell_model.iloc[:,4].dropna()  # Vector OF SOC versus OCV at 0 degree Celsius
-SOCrel= cell_model.iloc[:,5].dropna()  # Vector of change in SOC versus OCV per degree Celsius [1/C]
+OCV   = cell_model['OCV'].dropna()        # OCV vector at which SOC0 and SOCrel are stored
+OCV0  = cell_model['OCV0'].dropna()       # Vector of OCV versus SOC at 0 degree Celsius
+OCVrel= cell_model['OCVrel'].dropna()     # Vector of change in OCV versus SOC per degree Celsius [V/C]
+SOC   = cell_model['SOC'].dropna()        # SOC vector at which OCV0 and OCVrel are stored
+SOC0  = cell_model['SOC0'].dropna()       # Vector OF SOC versus OCV at 0 degree Celsius
+SOCrel= cell_model['SOCrel'].dropna()     # Vector of change in SOC versus OCV per degree Celsius [1/C]
 
 # Fields pertaining to the dynamic relationship:
-temps   = cell_model.iloc[:,6].dropna()   # Temperatures at which dynamic parameters are stored [C]
-Qparam  = cell_model.iloc[:,7].dropna()   # Capacity Q at each temperature [Ah]
-etaParam= cell_model.iloc[:,8].dropna()   # Coulombic efficiency eta at each temperature [unitless]
-Gparam  = cell_model.iloc[:,9].dropna()   # Hysteresis "gamma" parameter [unitless]
-MParam  = cell_model.iloc[:,10].dropna()  # Hysteresis M parameter [V]
-M0Param = cell_model.iloc[:,11].dropna()  # Hysteresis M0 parameter [V]
-R0Param = cell_model.iloc[:,12].dropna()  # Series resistance parameter R_0 [ohm]
-RCParam = cell_model.iloc[:,13].dropna()  # The R-C time constant parameter R_j C_j [s]
-RParam  = cell_model.iloc[:,14].dropna()  # Resistance R_j of R-C parameter [ohm]
-
+temps   = cell_model['temps'].dropna()    # Temperatures at which dynamic parameters are stored [C]
+QParam  = cell_model['QParam'].dropna()   # Capacity Q at each temperature [Ah]
+etaParam= cell_model['etaParam'].dropna() # Coulombic efficiency eta at each temperature [unitless]
+GParam  = cell_model['GParam'].dropna()   # Hysteresis "gamma" parameter [unitless]
+MParam  = cell_model['MParam'].dropna()   # Hysteresis M parameter [V]
+M0Param = cell_model['M0Param'].dropna()  # Hysteresis M0 parameter [V]
+R0Param = cell_model['R0Param'].dropna()  # Series resistance parameter R_0 [ohm]
+RCParam = cell_model['RCParam'].dropna()  # The R-C time constant parameter R_j C_j [s]
+RParam  = cell_model['RParam'].dropna()   # Resistance R_j of R-C parameter [ohm]
 
 
 def get_OCV0(z):
@@ -58,8 +57,8 @@ for time in range(int(sim_end_time)):
     # calculating next state 
     A_RC = np.exp(-dt / RCParam[curr_T])
     B_RC = 1 - A_RC
-    A_H = np.exp(-np.abs((etaParam[curr_T] * i[time] * Gparam[curr_T] * dt) / (Qparam[curr_T]*3600)))
-    z[time + 1] = z[time] + (-etaParam[curr_T] * dt / (Qparam[curr_T]*3600)) * i[time]
+    A_H = np.exp(-np.abs((etaParam[curr_T] * i[time] * GParam[curr_T] * dt) / (QParam[curr_T]*3600)))
+    z[time + 1] = z[time] + (-etaParam[curr_T] * dt / (QParam[curr_T]*3600)) * i[time]
     i_R[time + 1] = A_RC * i_R[time] + B_RC * i[time]
     h[time + 1] = A_H * h[time] + (A_H - 1) * np.sign(i[time])
 
@@ -69,20 +68,20 @@ for time in range(int(sim_end_time)):
     OCV = get_OCV0(z[time]) + get_OCVrel(z[time])*0.001*temps[curr_T]
     v[time + 1] = OCV + M0Param[curr_T]*s + MParam[curr_T]*h[time] - RParam[curr_T]*i_R[time] - R0Param[curr_T]*i[time]
 
-time_array = np.arange(0, sim_end_time+dt ,dt)
+t = np.arange(0, sim_end_time+dt ,dt)
 
 
 data = pd.DataFrame({
-    'time': time_array,
-    'SOC_true': z,
+    'time': t,
+    'current': i,
     'voltage': v,
-    'current': i
+    'soc': z,
 })
 data.to_csv('model_out/sim_data.csv', index=False) # This saves the simulated time, true SOC (z), voltage (v), and current (i) for use by the EKF script.
 
 
 # Plotting
-plt.plot(time_array, v, label="Output Voltage")
+plt.plot(t, v, label="Output Voltage")
 plt.title("Output Voltage (V)")
 plt.xlabel("Time (s)")
 plt.ylabel("Voltage (V)")
@@ -93,7 +92,7 @@ plt.legend()
 plt.show()
 
 
-plt.plot(time_array, z, label="State of charge")
+plt.plot(t, z, label="State of charge")
 plt.title("State of charge")
 plt.xlabel("Time (s)")
 plt.ylabel("SOC")
